21ab66ec1685e55b7e7f7c6c7f58bb00545584da
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2024-11-25 01:13:33 +0400

    Add project files.

---
+++ TimerTrigger/TimerTriggerReceive.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TimerTrigger
+{
+    public static class TimerTriggerReceive
+    {
+        [Function("TimerTriggerReceive")]
+        public static async Task Run(
+            [TimerTrigger("*/10 * * * * *")] TimerInfo myTimer
+            )
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            string queueName = "product-queue";
+
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (await queueClient.ExistsAsync())
+            {
+                var message = await queueClient.ReceiveMessageAsync();
+
+                if (message.Value != null)
+                    await queueClient.DeleteMessageAsync(message.Value.MessageId, message.Value.PopReceipt);
+            }
+
+        }
+    }
+}
+
---
+++ TimerTrigger/TimerTriggerSend.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TimerTrigger
+{
+    public static class TimerTriggerSend
+    {
+        [Function("TimerTriggerSend")]
+        public static async Task Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer)
+        {
+        
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            string queueName = "product-queue";
+
+            if (string.IsNullOrEmpty(connectionString)) return;
+
+           
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+            await queueClient.CreateIfNotExistsAsync();
+
+            if (await queueClient.ExistsAsync())
+            { 
+                var product = $"Product-{Guid.NewGuid()}";
+                await queueClient.SendMessageAsync(product);
+                
+            }
+            
+        }
+    }
+}
